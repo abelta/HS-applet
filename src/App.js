5998d3b59c98faf3e40e660a7431e7aa17e3ce6f
Abel <abel.tamayo@gmail.com>
2018-02-01 12:46:41 +0100

    Initial configuration.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div>
+    <header className="fl w-100">
+      <h1 className="f1">HS Applet</h1>
+    </header>
+  </div>
+);
+
+export default App;
